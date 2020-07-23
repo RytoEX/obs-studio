d829a87374c186137cb023deb37ff14b533290cd
Ryan Foster <RytoEX@gmail.com>
2020-07-23 00:11:56 -0400

    CI: Target macOS 10.13
    This commit also edits a cmake component.
    
    This commit also adds -Wno-error=deprecated-declarations to prevent the
    libcef_dll_wrapper build from failing due to deprecation warnings and
    errors.

--- CI/full-build-macos.sh
+++ CI/full-build-macos.sh
@@ -166,14 +166,14 @@ install_cef() {
     step "Fix tests..."
     # remove a broken test
     sed -i '.orig' '/add_subdirectory(tests\/ceftests)/d' ./CMakeLists.txt
-    # target 10.11
-    sed -i '.orig' s/\"10.9\"/\"10.11\"/ ./cmake/cef_variables.cmake
+    # target 10.13
+    sed -i '.orig' s/\"10.9\"/\"10.13\"/ ./cmake/cef_variables.cmake
     ensure_dir ./build
     step "Run CMAKE..."
     cmake \
         -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++"\
         -DCMAKE_EXE_LINKER_FLAGS="-std=c++11 -stdlib=libc++"\
-        -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 \
         ..
     step "Build..."
     make -j4
