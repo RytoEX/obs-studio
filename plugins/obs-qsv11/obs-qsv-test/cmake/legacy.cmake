eff40985f34d0f00efdf8967b1576b998a79e9f0
Gale <thy-lan.gale@intel.com>
2023-06-23 20:58:33 -0700

    obs-qsv11: Add VPL changes

--- plugins/obs-qsv11/obs-qsv-test/cmake/legacy.cmake
+++ plugins/obs-qsv11/obs-qsv-test/cmake/legacy.cmake
@@ -3,8 +3,11 @@ project(obs-qsv-test)
 include_directories(SYSTEM ${CMAKE_SOURCE_DIR}/libobs)
 
 add_executable(obs-qsv-test)
+
+find_package(VPL REQUIRED)
+
 target_sources(obs-qsv-test PRIVATE obs-qsv-test.cpp)
-target_link_libraries(obs-qsv-test d3d11 dxgi dxguid OBS::libmfx)
+target_link_libraries(obs-qsv-test d3d11 dxgi dxguid VPL::VPL)
 
 set_target_properties(obs-qsv-test PROPERTIES FOLDER "plugins/obs-qsv11")
 
