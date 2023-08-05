97470e94c89f8ff996467351c4bcca4333192336
Ryan Foster <ryan@obsproject.com>
2023-08-04 21:29:49 -0400

    fixup: handle debug libraries

--- cmake/finders/FindVPL.cmake
+++ cmake/finders/FindVPL.cmake
@@ -20,6 +20,8 @@ This module sets the following variables:
   True, if headers were found.
 ``VPL_VERSION``
   Detected version of found VPL headers.
+``VPL_LIBRARIES``
+  Libraries needed to link to VPL.
 
 Cache variables
 ^^^^^^^^^^^^^^^
@@ -28,8 +30,10 @@ The following cache variables may also be set:
 
 ``VPL_INCLUDE_DIR``
   Directory containing ``mfxdispatcher.h``.
-``VPL_LIB``
-  Path to the library component of VPL.
+``VPL_LIBRARY_RELEASE``
+  Path to the library component of VPL in non-debug configuration.
+``VPL_LIBRARY_DEBUG``
+  Optional path to the library component of VPL in debug configuration.
 
 #]=======================================================================]
 
@@ -40,7 +44,7 @@ The following cache variables may also be set:
 
 find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules(_VPL vpl)
+  pkg_search_module(_VPL QUIET vpl)
 endif()
 
 find_path(
@@ -51,18 +55,28 @@ find_path(
   DOC "VPL include directory")
 
 find_library(
-  VPL_LIB
+  VPL_LIBRARY_RELEASE
   NAMES ${_VPL_LIBRARIES} ${_VPL_LIBRARIES} vpl
   HINTS ${_VPL_LIBRARY_DIRS} ${_VPL_LIBRARY_DIRS}
   PATHS /usr/lib /usr/local/lib /opt/local/lib /sw/lib
   DOC "VPL library location")
 
+find_library(
+  VPL_LIBRARY_DEBUG
+  NAMES ${_VPL_LIBRARIES} ${_VPL_LIBRARIES} vpld
+  HINTS ${_VPL_LIBRARY_DIRS} ${_VPL_LIBRARY_DIRS}
+  PATHS /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+  DOC "VPL debug library location")
+
+include(SelectLibraryConfigurations)
+select_library_configurations(VPL)
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   VPL
-  REQUIRED_VARS VPL_LIB VPL_INCLUDE_DIR
+  REQUIRED_VARS VPL_LIBRARY VPL_INCLUDE_DIR
   VERSION_VAR VPL_VERSION REASON_FAILURE_MESSAGE "${VPL_ERROR_REASON}")
-mark_as_advanced(VPL_INCLUDE_DIR VPL_LIB)
+mark_as_advanced(VPL_INCLUDE_DIR VPL_LIBRARY)
 unset(VPL_ERROR_REASON)
 
 if(EXISTS "${VPL_INCLUDE_DIR}/vpl/mfxdefs.h")
@@ -84,23 +98,39 @@ endif()
 if(CMAKE_HOST_SYSTEM_NAME MATCHES "Darwin|Windows")
   set(VPL_ERROR_REASON "Ensure that obs-deps is provided as part of CMAKE_PREFIX_PATH.")
 elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux|FreeBSD")
-  set(VPL_ERROR_REASON "Ensure vpl headers are available in local library paths.")
+  set(VPL_ERROR_REASON "Ensure VPL headers are available in local library paths.")
 endif()
 
 if(VPL_FOUND)
   set(VPL_INCLUDE_DIRS ${VPL_INCLUDE_DIR})
-  set(VPL_LIBRARIES ${VPL_LIB})
+  set(VPL_LIBRARIES ${VPL_LIBRARY})
 
   if(NOT TARGET VPL::VPL)
-    if(IS_ABSOLUTE "${VPL_LIBRARIES}")
+    if(IS_ABSOLUTE "${VPL_LIBRARY_RELEASE}")
       add_library(VPL::VPL UNKNOWN IMPORTED)
-      set_target_properties(VPL::VPL PROPERTIES IMPORTED_LOCATION "${VPL_LIBRARIES}")
+      set_target_properties(VPL::VPL PROPERTIES IMPORTED_LOCATION "${VPL_LIBRARY_RELEASE}")
     else()
       add_library(VPL::VPL INTERFACE IMPORTED)
-      set_target_properties(VPL::VPL PROPERTIES IMPORTED_LIBNAME "${VPL_LIBRARIES}")
+      set_target_properties(VPL::VPL PROPERTIES IMPORTED_LIBNAME "${VPL_LIBRARY_RELEASE}")
     endif()
 
-    set_target_properties(VPL::VPL PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${VPL_INCLUDE_DIRS}")
+    set_target_properties(
+      VPL::VPL
+      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${VPL_INCLUDE_DIRS}"
+      VERSION ${VPL_VERSION}
+      IMPORTED_CONFIGURATIONS Release)
+
+    if(VPL_LIBRARY_DEBUG)
+      if(IS_ABSOLUTE "${VPL_LIBRARY_DEBUG}")
+        set_target_properties(VPL::VPL PROPERTIES IMPORTED_LOCATION_DEBUG "${VPL_LIBRARY_DEBUG}")
+      else()
+        set_target_properties(VPL::VPL PROPERTIES IMPORTED_LIBNAME_DEBUG "${VPL_LIBRARY_DEBUG}")
+      endif()
+      set_property(
+        TARGET VPL::VPL
+        APPEND
+        PROPERTY IMPORTED_CONFIGURATIONS Debug)
+    endif()
   endif()
 endif()
 
